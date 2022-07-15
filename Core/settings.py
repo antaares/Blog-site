289f0672cdb1ed6ae01935e9b56150264393047c
antaares <jrismoilov@gmail.com>
2022-07-15 15:07:04 +0500

    ready to deploy

--- Core/settings.py
+++ Core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3j+azs536-u$2=pe-@tcdi*v7ob+p)q%23+)-7l!v6t)w6$!1c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
